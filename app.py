f3dfa11714b522e776bfa43eee5bf32fb8dd98a7
jhonny rojas <jhonnyrojasflo@gmail.com>
2024-05-16 09:43:17 -0400

    Implementacion del la validacion de datos del inisio sesion usuario

--- app.py
+++ app.py
@@ -27,73 +27,7 @@ app.config['MYSQL_PASSWORD'] = ''  # Asegúrate de ingresar tu con
 app.config['MYSQL_DB'] = 'campus_alalay'
 mysql=MySQL(app)
 
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-GOOGLE_CLIENT_ID = "767449868214-nqko9t273dmno2ehkht331o9070bc78h.apps.googleusercontent.com"
-client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
-
-flow = Flow.from_client_secrets_file(
-    client_secrets_file=client_secrets_file,
-    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="http://127.0.0.1:5000/callback"
-)
-
-def login_is_required(function):
-    def wrapper(*args, **kwargs):
-        if "google_id" not in session:
-            return abort(401)  # Authorization required
-        else:
-            return function()
-
-    return wrapper
-
-@app.route("/callback")
-def callback():
-    flow.fetch_token(authorization_response=request.url)
-
-    if not session["state"] == request.args["state"]:
-        abort(500)  # State does not match!
-
-    credentials = flow.credentials
-    request_session = requests.session()
-    cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
-
-    id_info = id_token.verify_oauth2_token(
-        id_token=credentials._id_token,
-        request=token_request,
-        audience=GOOGLE_CLIENT_ID
-    )
-
-    session["google_id"] = id_info.get("sub")
-    session["name"] = id_info.get("name")
-    session["email"]=id_info.get("email")
-    session["picture"]=id_info.get("picture")
-    return redirect("/protected_area")
-
-@app.route("/logout")
-def logout():
-    session.clear()
-    return redirect("/login")
-
-@app.route("/login_google")
-def login_google():
-    authorization_url, state = flow.authorization_url()
-    session["state"] = state
-    return redirect(authorization_url)
-
-@app.route("/protected_area")
-@login_is_required
-def protected_area():
-   nombre=session.get('name')
-   foto=session.get('picture')
-   correo=session.get('email')
-   cur=mysql.conntection.cursor()
-   conn=cur
-   cur.execute('select*from registro_docentes where nombre_completo=%s and correo_electronico=%s',(nombre,correo,))
-   docentes=cur.fetchone()
-   if docentes is None :
-       cur.execute('insert into estudiante(nombre,correo,contrasena)values(%s)')
 @app.route('/')
 def landing():
     # session.clear()
